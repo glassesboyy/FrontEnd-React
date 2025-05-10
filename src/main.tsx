32b72c59f12a0e3e761aa9a284c7fd9b54f1b420
glassesboyy <aliperwira26@gmail.com>
2025-05-10 22:06:12 +0700

    install library tanstack query untuk mengelola data server (api), membuat auth context untuk cek status auth pada aplikasi (dijadikan state management global), lalu membuat hooks untuk auth (auth user, login, logout, register)

--- src/main.tsx
+++ src/main.tsx
@@ -5,10 +5,23 @@ import App from "./App.tsx";
 //import BrowserRouter dari react router
 import { BrowserRouter } from "react-router";
 
+//import QueryClient dan QueryClientProvider dari react-query
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+//init QueryClient
+const queryClient = new QueryClient();
+
+//import AuthProvider
+import { AuthProvider } from "./context/AuthContext";
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+          <App />
+        </QueryClientProvider>
+      </BrowserRouter>
+    </AuthProvider>
   </StrictMode>
 );
